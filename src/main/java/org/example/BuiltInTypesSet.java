3d686806a9976813caccefe92606a3c41bdb8578
DQYuan <932087612@qq.com>
2022-01-03 00:33:18 +0800

    delete unuse type

--- src/main/java/org/example/BuiltInTypesSet.java
+++ src/main/java/org/example/BuiltInTypesSet.java
@@ -2,10 +2,6 @@ package org.example;
 
 public class BuiltInTypesSet {
 
-    public static final String BYTE = "byte";
-
-    public static final String SHORT = "short";
-
     public static final String INT = "int";
 
     public static final String LONG = "long";
@@ -14,24 +10,16 @@ public class BuiltInTypesSet {
 
     public static final String DOUBLE = "double";
 
-    public static final String BOOLEAN = "boolean";
-
-    public static final String CHAR = "char";
-
     public static final String STRING = "String";
 
     public static final String FUNCTION = "Function";
 
     public static boolean isBuiltInType(String lexeme) {
         switch (lexeme) {
-            case BYTE:
-            case SHORT:
             case INT:
             case LONG:
             case FLOAT:
             case DOUBLE:
-            case BOOLEAN:
-            case CHAR:
             case STRING:
             case FUNCTION:
                 return true;
